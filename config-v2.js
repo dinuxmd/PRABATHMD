6a6a20fe2dcec2ac7c10b2e8ab7486cf2ecd57b1
dinuxmd <Pavangimhana88@gmail.com>
2024-10-13 11:24:50 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,7 +1,7 @@
 const GITHUB_AUTH_TOKEN = "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"
 const BOT_NUMBER = "94763809289"
 const SESSION_ID = "PRABATH-MD~1m0F2L5A#rPG8uPIaGdgtxUIDIrswOD-d7S2wjo1jPkqjnW7cun0"
-const GITHUB_USERNAME = "dinuxmd"
+const GITHUB_USER_NAME = "dinuxmd"
 
 module.exports = {
 GITHUB_AUTH_TOKEN,
